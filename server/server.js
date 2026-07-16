eac27cacbd08bce8ba948f6b552fe1c0add17dbf
archit <architawasthims7@gmail.com>
2026-07-16 10:06:38 +0530

    Setup MongoDB Atlas Connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log("==================================");
-  console.log(`🚀 AutoFlow Backend Running`);
-  console.log(`🌐 http://localhost:${PORT}`);
-  console.log("==================================");
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log("==================================");
+    console.log("🚀 AutoFlow Backend Running");
+    console.log(`🌐 http://localhost:${PORT}`);
+    console.log("==================================");
+  });
+};
+
+startServer();
